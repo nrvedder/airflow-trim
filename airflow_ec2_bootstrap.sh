a87d39313f7d6fdac1717e8edd4e4d9d8bf268d6
Nick Vedder <nick.vedder@nvedder-mpb.local>
2020-02-02 10:46:51 -0700

    commenting out sql_alchemy_conn; adding to bootstrap

--- airflow_ec2_bootstrap.sh
+++ airflow_ec2_bootstrap.sh
@@ -10,6 +10,7 @@ sudo gpasswd -a $USER docker
 
 # scp credentials up
 
+sudo yum install git -y
 
 aws ecr get-login --region us-east-2
 
@@ -22,9 +23,11 @@ sudo yum install mysql mysql-server -y
 # https://acloudxpert.com/how-to-install-docker-compose-on-amazon-linux-ami/
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 
+sudo chmod +x /usr/local/bin/docker-compose
 
-# Start docker compose
-docker-compose -f docker-compose.yml up -d
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# Start docker-compose
+sudo docker-compose -f docker-compose.yml up -d
 
 # Change RDS database in airflow.cfg, airflow.cpzddhbjekdf.us-east-2.rds.amazonaws.com. Change docker image in docker-compose.yml
